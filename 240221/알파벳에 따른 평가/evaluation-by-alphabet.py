33362e9a7104c1c1ee3cdc1a4b3bb67c82a809df
JAEWOO CHO <65455339+zaew0o0s@users.noreply.github.com>
2024-02-21 21:24:08 +0900

    Title: 알파벳에 따른 평가, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240221/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,13 @@
+word = input()
+if word == "S":
+    print("Superior")
+elif word == "A":
+    print("Excellent")
+elif word == "B":
+    print("Good")
+elif word == "C":
+    print("Usually")
+elif word == "D":
+    print("Effort")
+else:
+    print("Failure")
