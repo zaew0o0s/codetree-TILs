0c75ae0dd4a72d4df647aad02f15d2ed6d4aa1c4
JAEWOO CHO <65455339+zaew0o0s@users.noreply.github.com>
2024-02-24 18:55:01 +0900

    Title: 두 사람, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240224/두 사람/two-person.py
+++ 240224/두 사람/two-person.py
@@ -2,4 +2,7 @@ a_age, a_sex = input().split()
 b_age, b_sex = input().split()
 a_age, b_age = int(a_age), int(b_age)
 
-print(int(((a_age >= 19 or b_age >= 19) and (a_sex == "M" or b_sex == "M"))))
+if (a_age >= 19 or b_age >= 19) and (a_sex == "M" or b_sex == "M"):
+    print(1)
+else:
+    print(0)
